7d3da4e24e26f7d7364abc6eb976f30e8f4f71a6
Alexandru Emil Lupu <contact@alecslupu.ro>
2023-03-16 10:35:30 +0200

    Remove unused codebase

--- app/cells/decidim/slider/tabs/image/settings_cell.rb
+++ app/cells/decidim/slider/tabs/image/settings_cell.rb
@@ -7,16 +7,8 @@ module Decidim
         class SettingsCell < Decidim::ViewModel
           alias form model
 
-          def content_block
-            options[:content_block]
-          end
-
           def locales
-            @locales ||= if @template.respond_to?(:available_locales)
-                           Set.new([@template.current_locale] + @template.available_locales).to_a
-                         else
-                           Decidim.available_locales
-                         end
+            current_organization.available_locales
           end
         end
       end
